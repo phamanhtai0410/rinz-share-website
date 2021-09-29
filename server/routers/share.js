61b3fc7a97078302d0c9204c7a6862be25f61978
PAT <phamanhtai0410@gmail.com>
2021-09-29 13:38:09 +0700

    Implement Dynamnic Link for Share Website

--- server/routers/share.js
+++ server/routers/share.js
@@ -76,6 +76,9 @@ const get_dyanmic_link = link =>
     var options = {
       method: 'POST',
       url: `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${FIREBASE_KEY}`,
+      headers: {
+        "content-type": "application/json",
+      },
       json: {
         "dynamicLinkInfo": {
           "domainUriPrefix": "https://rinzworld.page.link",
@@ -94,9 +97,11 @@ const get_dyanmic_link = link =>
       if (error) {
         reject();
       }
-      let response_json = JSON.parse(response.body);
-      console.log(response_json.status);
-      if (response_json.status === 1) {
+  
+      let response_json = response.body;
+      // JSON.parse(response.body);
+      // console.log('Body: ', response_json.shortLink);
+      if (response_json) {
         resolve(response_json);
       }
       resolve({});
@@ -116,11 +121,13 @@ router.get('/artist/:artist_id', async (req, res) => {
   const { artist_id } = req.params;
   // const id = artist_id.split('-')[0];
   const detail = await get_artist_detail(artist_id);
+  const dynamic_link_json = await get_dyanmic_link(`/artist/${artist_id}`);
   console.log(detail);
   res.render('share/artist/index', {
     detail: detail,
     type: 'artist',
-    id: detail.id
+    id: detail.id,
+    dynamic_link: dynamic_link_json.shortLink
   });
 });
 
@@ -131,13 +138,25 @@ router.get('/track/:track_id', async (req, res) => {
   const { track_id } = req.params;
   // const id = artist_id.split('-')[0];
   const detail = await get_track_detail(track_id);
-  // const dynamic_link_json = await get_dyanmic_link(`/track/${track_id}`);
-  console.log(detail);
+  const dynamic_link_json = await get_dyanmic_link(`/track/${track_id}`);
+  console.log('- Track dynamic link: ', dynamic_link_json);
+  console.log('- Track detail: ', detail);
+
   res.render('share/track/index', {
     detail: detail,
     type: 'track',
-    id: detail.id
+    id: detail.id,
+    dynamic_link: dynamic_link_json.shortLink
   });
+});
+
+/////
+/// Test route
+//
+router.get('/test/:track_id', async (req, res) => {
+  const { track_id } = req.params;
+  const dynamic_link_json = await get_dyanmic_link(`/track/${track_id}`);
+  console.log('dynamic link: ', dynamic_link_json);
 });
 
 ////
@@ -172,11 +191,13 @@ router.get('/event/:event_id', async (req, res) => {
   const { event_id } = req.params;
   // const id = artist_id.split('-')[0];
   const detail = await get_event_detail(event_id);
+  const dynamic_link_json = await get_dyanmic_link(`/event/${event_id}`);
   console.log(detail);
   res.render('share/event/index', {
     detail: detail,
     type: 'event',
-    id: detail.id
+    id: detail.id,
+    dynamic_link: dynamic_link_json.shortLink
   });
 });
 
